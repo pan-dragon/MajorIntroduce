d2a677870cc9a67a25705a65ce2cc39a67d5b194
Administrator <1171867004@qq.com>
2017-01-29 21:38:55 +0800

    增加底部菜单
    Signed-off-by: 韦博文 <1171867004@qq.com>

--- app/src/main/java/com/gxdlzyjsxy/majorintroduce/activities/MainActivity.java
+++ app/src/main/java/com/gxdlzyjsxy/majorintroduce/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.gxdlzyjsxy.majorintroduce;
+package com.gxdlzyjsxy.majorintroduce.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.gxdlzyjsxy.majorintroduce.R;
 
 public class MainActivity extends AppCompatActivity
 {
